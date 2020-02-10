f51c3229fdbaaa52c51e86479899be89a20019a3
Truoc Phan <57470560+truocphan@users.noreply.github.com>
2020-02-11 01:37:16 +0700

    Update secretRecovery.py

--- secretRecovery.py
+++ secretRecovery.py
@@ -1,28 +1,43 @@
 import time
 
-secret_length = 64
-leaks = ["01a", "049", "0b1", "0c5", "12a", "1ad", "1b0", "1c6", "1d1", "2a9", "2ba", "2fd", "3b8", "40c", "489", "494", "4a6", "4b4", "4d4", "4eb", "51b", "57c", "60b", "68c", "69e", "6b6", "740", "77c", "7c9", "7c8", "84a", "851", "897", "8c2", "92f", "94d", "977", "9e0", "9f1", "a68", "a85", "a92", "ad4", "b04", "b1d", "b48", "b69", "b74", "b84", "ba8", "c2b", "c57", "c6b", "c9f", "d1c", "d3b", "d4b", "d4e", "e01", "eb7", "f12", "fd3"]
-
-
+data_leaks = ["051", "0bc", "0bb", "192", "1f4", "28b", "2da", "339", "37d", "39b", "3f4", "3f6", "465", "4b3", "4e2", "51f", "53f", "56a", "5d7", "5e6", "605", "60b", "65e", "666", "66e", "6a1", "6ad", "6ee", "756", "760", "7d5", "853", "8b8", "92d", "93f", "9b7", "9ba", "a14", "a37", "a9b", "ab0", "ad9", "b0b", "b33", "b75", "b85", "bab", "ba9", "bba", "bc6", "c66", "d5d", "d76", "d93", "da3", "e28", "e4e", "e60", "ee4", "f46", "f4b", "f6a"]
 
 def secretRecovery(start, leaks):
 	print("["+time.strftime("%d/%m/%Y %H:%M:%S", time.localtime(time.time()))+"] " + start)
-	time.sleep(0.2)
-	try:
-		leaks.remove(start[-3:])
-		if len(leaks) == 0:
-			print("==> YOUR SECRET IS: {}\n".format(start))
-		for j in [i for i in leaks if i[:2] == start[-2:]]:
-			start += j[-1]
-			secretRecovery(start, leaks)
-	except Exception as e:
-		print("==> Your secret is not found!!!\n")
+	time.sleep(0.1)
+	# Xoa bo 3 ky tu da tim duoc khoi data_leaks
+	leaks.remove(start[-3:])
+	# Neu data_leaks khong con bo 3 ky tu nao thi luc gia tri start co the la SECRET can tim
+	if len(leaks) == 0:
+		secret.append(start)
+		print("==> THIS IS A SECRET.\n")
+	# Neu data_leaks van con thi tiep tuc tim cac ky tu ke tiep de tao thanh SECRET
+	else:
+		# Bo 3 ky tu ke tiep phai co 2 ky tu dau trung voi 2 ky tu cuoi cua gia tri start
+		next = [i for i in leaks if i[:2] == start[-2:]]
+		# Neu khong co bo 3 ky tu nao thoa dieu kien thi gia tri start se cham dut va khong phai la SECRET
+		if len(next) == 0:
+			print("==> This is not a secret!!!\n")
+		# Nguoc lai, neu co thi lap qua tung bo 3 ky tu thoa dieu kien tren
+		else:
+			for i in next:
+				secretRecovery(start + i[-1], leaks[:])
 
 
 
-if len(leaks) == (secret_length - 2):
-	starts = [x for x in leaks if x[:2] not in [y[1:] for y in leaks]]
-	for start in starts:
-		secretRecovery(start, leaks)
+secret = list()
+# Tim bo 3 ky tu bat dau cua SECRET tu data_leaks (2 ky tu dau khong duoc trung voi 2 ki tu cuoi cua cac bo 3 ky tu con lai trong data_leaks)
+start = [x for x in data_leaks if x[:2] not in [y[1:] for y in data_leaks]]
+# Neu ton tai
+if len(start) == 1:
+	secretRecovery(start[0], data_leaks[:])
+	print("YOUR SECRET IS:")
+	for i in secret:
+		print("[+] " + i)
+# Neu khong thi gia su tung bo 3 ky tu trong data_leaks la bo 3 ky tu bat dau cua SECRET, sau do Brute force 
 else:
-	exit("ERROR!!!")
+	for start in data_leaks:
+		secretRecovery(start, data_leaks[:])
+		print("YOUR SECRET IS:")
+		for i in secret:
+			print("[+] " + i)
